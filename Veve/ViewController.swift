43377b3972c612f19feb939e76008707801deb0f
Jamal <16698402+trujamal@users.noreply.github.com>
2018-10-31 19:50:15 -0500

    Initial Commit

---
+++ Veve/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Veve
+//
+//  Created by Jamal Rasool on 10/31/18.
+//  Copyright © 2018 Jamal Rasool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
