c0cc2e778e71cad48bd21fb8c366ddfda5c93bd0
Jamal <16698402+trujamal@users.noreply.github.com>
2018-11-08 16:13:20 -0600

    Updates to internal functions

--- Veve/ViewController.swift
+++ Veve/ViewController.swift
@@ -13,8 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        testExample()
     }
-
+    
+    fileprivate func testExample() {
+        var first_array = 0
+        var second_array = 0
+        
+        var arr : [[Int]] = [[11,2,4], [4,5,6] , [10,8,-12]]
+        let arrayValue = arr.count
+        
+        for i in 0..<arrayValue {
+            first_array += arr[i][i]
+            
+        }
+        
+        for i in (0..<arrayValue) {
+            second_array += arr[i][arrayValue - i - 1]
+            
+        }
+        
+        let sum = abs(first_array - second_array)
+        
+        print(sum)
+        
+    }
 
 }
 
